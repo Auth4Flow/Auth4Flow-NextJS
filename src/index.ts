fbf8dbf586ec94b3f2ac50c46aa114b476ae2750
BoiseITGuru <dev@boiseitguru.com>
2023-07-15 23:35:39 -0600

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as withSessionPermission } from "./middleware/withSessionPermission";
 export { Forge4FlowClient as Forge4FlowServer } from "@forge4flow/forge4flow-node";
+export * from "./middleware/withSessionPermission";
 export type * from "@forge4flow/forge4flow-node/src/types";
