03a50cb6bb105ec6ee98eaf824c4eaf7757ea14d
BoiseITGuru <dev@boiseitguru.com>
2023-08-21 11:55:24 -0600

    withSessionFeature fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Forge4FlowClient as Forge4FlowServer } from "@forge4flow/forge4flow-node";
 export * from "@forge4flow/forge4flow-react/dist/index";
 export * from "./middleware/withSessionPermission";
-export * from "./middleware/withSessionPermission";
+export * from "./middleware/withSessionFeature";
 export * from "@forge4flow/forge4flow-node/dist/types";
